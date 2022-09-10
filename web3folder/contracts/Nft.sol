4428097c92330063b39831e059b152a418d8a2b6
kunle niyi <58327707+adekniyi@users.noreply.github.com>
2022-09-10 16:24:01 +0000

    Feat: Get all nfts and create nft

--- web3folder/contracts/Nft.sol
+++ web3folder/contracts/Nft.sol
@@ -15,27 +15,31 @@ contract Nft{
     }
 
     struct NftDetails{
-        uint id;
+        bytes16 id;
         uint price;
         bytes32 proof;
         address owner;
-        // bytes32 description;
-        // uint DateCreated;
+        bytes32 description;
+        uint dateCreated;
         State state;
+        bytes32 name;
     }
 
     mapping(bytes16 => bytes32) OwnerNfthash;
     mapping(bytes32 => NftDetails) ownerNft;
 
+    NftDetails[] AllNfts;
 
     /// You own this Nft already
     error NftHasOwner();
 
     function PurchaseNft(
         bytes16 nftId, //0x00000000000000000000000000003130
-        bytes32 proof //0x0000000000000000000000000000313000000000000000000000000000003130
+        bytes32 proof, //0x0000000000000000000000000000313000000000000000000000000000003130
+        bytes32 description,
+        bytes32 name
     )
-    external payable{
+    public payable{
         bytes32 nftHash = keccak256((abi.encodePacked(nftId,msg.sender)));
 
         if(hasNftOwnership(nftHash)){
@@ -49,11 +53,14 @@ contract Nft{
         if(previosHash == ""){
             OwnerNfthash[nftId] = nftHash;
             ownerNft[nftHash] = NftDetails({
-                id: 1,
+                id: nftId,
                 price : msg.value,
                 proof: proof,
                 owner: msg.sender,
-                state: State.Purchased
+                state: State.Purchased,
+                description: description,
+                dateCreated: block.timestamp,
+                name: name
             });
         }else{
             NftDetails memory previousNft = ownerNft[previosHash];       
@@ -65,15 +72,37 @@ contract Nft{
                 price : previousNft.price,
                 proof: previousNft.proof,
                 owner: msg.sender,
-                state: previousNft.state
+                state: previousNft.state,
+                description: previousNft.description,
+                dateCreated: previousNft.dateCreated,
+                name: previousNft.name
             });
             
             delete ownerNft[previosHash];
         }
     }
-    // function AddNft(string memory ntfName, uint64 price, string memory nftDescription) public returns(bool success){
+    function AddNft(bytes32 ntfName, bytes32 nftDescription, bytes32 proof, bytes16 nftId) 
+    external payable
+    returns(bool success){
 
-    // }
+        NftDetails memory nft = NftDetails({
+                id: nftId,
+                price : msg.value,
+                proof: proof,
+                owner: msg.sender,
+                state: State.Purchased,
+                description: nftDescription,
+                dateCreated: block.timestamp,
+                name: ntfName
+        });
+
+        AllNfts.push(nft);
+
+        PurchaseNft(nft.id,nft.proof,nft.description,nft.name);
+
+        return true;
+
+    }
 
     function GetNftHash(bytes16 nftId)
     external
@@ -91,6 +120,13 @@ contract Nft{
         return ownerNft[nfthash];
     }
 
+    function GetAllNfts()
+    public
+    view
+    returns
+    (NftDetails[] memory){
+        return AllNfts;
+    }
 
     function hasNftOwnership(bytes32 nfthash)
     private 
